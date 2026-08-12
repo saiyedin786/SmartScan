4b04551eca336160b9747b41bfc2d023f9e93013
saiyedin786 <saiyedin786@gmail.com>
2026-08-13 01:18:05 +0530

    Pin Gradle version to 8.4

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,13 +1,13 @@
 @echo off
 echo =======================================================
-echo     SmartScan - Upload Plugin ID Fix to GitHub
+echo     SmartScan - Upload Gradle 8.4 Version Pin Fix
 echo =======================================================
 echo.
 
 cd /d "%~dp0"
 
 git add .
-git commit -m "Fix Kotlin plugin IDs to org.jetbrains.kotlin.android and org.jetbrains.kotlin.kapt"
+git commit -m "Pin Gradle version to 8.4 for AGP 8.1.4 compatibility"
 git push origin main
 
 echo.
