a29b687e5337a7cf3ed93ac1ad795d22d2694a12
saiyedin786 <saiyedin786@gmail.com>
2026-08-13 01:00:04 +0530

    Use setup-gradle cloud action

--- push_to_github.bat
+++ push_to_github.bat
@@ -1,13 +1,14 @@
 @echo off
 echo =======================================================
-echo     SmartScan - Upload JVM Opts Fix to GitHub
+echo     SmartScan - Upload Wrapper Jar Fix to GitHub
 echo =======================================================
 echo.
 
 cd /d "%~dp0"
 
+git add -f gradle/wrapper/gradle-wrapper.jar
 git add .
-git commit -m "Fix DEFAULT_JVM_OPTS ClassNotFoundException in gradlew"
+git commit -m "Use setup-gradle action and include wrapper jar"
 git push origin main
 
 echo.
