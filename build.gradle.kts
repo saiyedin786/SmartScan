49e944b1f7de3e459b4ea6166b79f2cedaa1fb13
saiyedin786 <saiyedin786@gmail.com>
2026-08-13 01:26:02 +0530

    Match Kotlin version 1.9.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,6 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.1.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
     }
 }
--- push_to_github.bat
+++ push_to_github.bat
@@ -1,13 +1,13 @@
 @echo off
 echo =======================================================
-echo     SmartScan - Upload Final Manifest Icon Fix
+echo     SmartScan - Upload Kotlin Version Match Fix
 echo =======================================================
 echo.
 
 cd /d "%~dp0"
 
 git add .
-git commit -m "Fix AndroidManifest app icon resource reference"
+git commit -m "Match Kotlin version 1.9.20 with Compose compiler 1.5.4"
 git push origin main
 
 echo.
